5dbfd09cea4fe5dbdb7547184de59685019dfd00
Lordestik <lordestik@mail.ru>
2020-12-13 16:21:12 +0300

    add first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+#1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+age = 34
+name = "Leonid"
+print(f"My name is {name}. I am {age} years old.")
+
+your_name = input("What's your name ? ")
+your_age = input("How old are you ? ")
+
+print(f"Your name is {your_name}. You are {your_age} years old.")
